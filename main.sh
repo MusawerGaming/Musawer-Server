80000082ac9110ae293474e0dade0f79808dd90a
MusawerGaming <118316797+MusawerGaming@users.noreply.github.com>
2025-12-20 08:26:03 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,7 +2,8 @@
 echo starting...
 cd velocity
 
-MTOD="${MTOD:-'Built By Musawer'}"
+MTOD="${MTOD:-'§7§lMusawer Server §4• §cCome Play Now
+ §8§lBuilt By §4Musawer}"
 SERVERNAME="${SERVERNAME:-'Musawer Server'}"
 
 if [ -n "$ICON" ]; then
